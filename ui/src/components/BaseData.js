9712ecda78744fdc6a72d0dd15391252fb414411
sylnsr <git@idaz.co>
2020-07-29 14:06:39 -0600

    check session when initializing any form

--- ui/src/components/BaseData.js
+++ ui/src/components/BaseData.js
@@ -18,17 +18,25 @@ export default {
     }
   },
   mounted () {
-    Server.check_access_rights(this.model, 'read').then(r => {
-      this.canRead = r
-      return Server.check_access_rights(this.model, 'write').then(r => {
-        this.canWrite = r
-        return Server.check_access_rights(this.model, 'unlink').then(r => {
-          this.canDelete = r
-          // console.log('permissions for', this.model, ':', this.canRead, this.canWrite, this.canDelete)
-          EventBus.$emit('access-rights', {
-            read: this.canRead,
-            write: this.canWrite,
-            delete: this.canDelete
+    // first we make sure the session is OK - this will also update profile data in Vuex store
+    Server.checkSession().then(r => {
+      if (!r) {
+        this.$router.push('/')
+        return
+      }
+      // session is OK so next we can check permissions based model and on data in Vuex Store
+      Server.check_access_rights(this.model, 'read').then(r => {
+        this.canRead = r
+        return Server.check_access_rights(this.model, 'write').then(r => {
+          this.canWrite = r
+          return Server.check_access_rights(this.model, 'unlink').then(r => {
+            this.canDelete = r
+            console.log('permissions for', this.model, ':', this.canRead, this.canWrite, this.canDelete)
+            EventBus.$emit('access-rights', {
+              read: this.canRead,
+              write: this.canWrite,
+              delete: this.canDelete
+            })
           })
         })
       })
